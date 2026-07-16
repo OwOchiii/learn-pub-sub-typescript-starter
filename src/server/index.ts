e11f4e658146bdf9b3b50db76112facb3afd1572
Nguyen Vu An <nguyenvuan2005@gmail.com>
2026-07-16 17:11:06 +0700

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import {getInput, printServerHelp} from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -27,6 +28,34 @@ async function main() {
 
   await publishJSON<PlayingState>(confirmChannel, ExchangePerilDirect, PauseKey, { isPaused: true });
   console.log("Published pause message");
+
+  printServerHelp()
+
+  while (true) {
+    const input = await getInput()
+    if (input.length === 0) {
+      continue;
+    }
+    else if (input[0] === "pause") {
+      console.log("Pausing game...");
+      await publishJSON<PlayingState>(confirmChannel, ExchangePerilDirect, PauseKey, { isPaused: true });
+    }
+    else if (input[0] === "resume") {
+      console.log("Resuming game...");
+      await publishJSON<PlayingState>(confirmChannel, ExchangePerilDirect, PauseKey, { isPaused: false });
+    }
+    else if (input[0] === "quit") {
+      console.log("Quitting...");
+      await conn.close();
+      process.exit(0);
+    }
+    else if (input[0] === "help") {
+      printServerHelp();
+    }
+    else {
+      console.log("Unknown command. Type 'help' for available commands.");
+    }
+  }
 }
 
 main().catch((err) => {
